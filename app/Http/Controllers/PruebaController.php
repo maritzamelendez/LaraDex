88887c8af3853feb30ec3a83ff1d4b17db12fdb3
maritzamelendez <maritza.melendez@uimqroo.edu.mx>
2020-02-12 12:58:44 -0500

    Initial Commit

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LaraDex\Http\Controllers;
+use LaraDex\Http\Controllers\Controller;
+
+/**
+ * 
+ */
+class PruebaController extends Controller
+{
+	
+	public function prueba($param)
+	{
+		return 'Estoy dentro de PruebaController y recibi este parametro'. $param;
+	}
+}
---
+++ resources/views/trainers/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Trainers Create')
+
+@section('content')
+
+	<div class="form-group">
+		<label for="">Nombre</label>
+		<input type="text" class="form-control">
+	</div>
+	<button type="submit" class="btn btn-primary">Guardar</button>
+
+@endsection
+
+
